c4a8acc938185e4253afad48f6599526ce35b024
Tomer Dror <drortomer9@gmail.com>
2024-11-20 13:29:08 +0200

    adding a test to playwright

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -6,4 +6,19 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('navigate from the main page to the page of a particular Pokemon', async ({ page }) => {
+    await page.goto('')
+
+    // Assert that the list of Pokemon is visible
+    await expect(page.locator('text=ivysaur')).toBeVisible()
+
+    // Click on a Pokemon name to navigate to the details page
+    await page.click('text=ivysaur')
+
+    await expect(page.locator('text=chlorophyll')).toBeVisible()
+
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
 })
