a596f7ed5c0d353576987b6596609d50a508cfd6
Tomer Dror <drortomer9@gmail.com>
2024-11-21 15:02:21 +0200

    reg url

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    baseURL: 'https://full-stack-open-pokedex-8pfy.onrender.com',
+    baseURL: 'http://127.0.0.1:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
